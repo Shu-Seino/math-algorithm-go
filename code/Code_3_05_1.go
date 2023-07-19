a9f0e26fcb5f477e7360572c77a7f0dab17e1528
Shu-Seino <shuuublog4649@gmail.com>
2023-07-19 05:43:25 +0000

    Code_3_05_1.goを追加

---
+++ code/Code_3_05_1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	N := 100 // N is the number of trials (change as needed)
+	M := 0
+
+	for i := 1; i <= N; i++ {
+		px := rand.Float64() // Generate a random number between 0 and 1
+		py := rand.Float64() // Generate a random number between 0 and 1
+
+		// The distance from the origin is sqrt(px * px + py * py)
+		// If this value is less than or equal to 1, it's within the circle
+		if px*px+py*py <= 1.0 {
+			M += 1
+		}
+	}
+
+	fmt.Printf("%.12f\n", 4.0*float64(M)/float64(N))
+}
